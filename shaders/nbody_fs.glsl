359a4d3da680069a1c60f9d7709db90d453207f8
devpack <anthony.prieur@gmail.com>
2024-01-04 19:41:08 +0100

    init

--- shaders/nbody_fs.glsl
+++ shaders/nbody_fs.glsl
@@ -12,6 +12,14 @@ void main() {
 	//fragColor = vec4(col, col, col, 1.0);
 
 	//fragColor = vec4(body_color_dist, body_color_dist, body_color_dist, 1.0);
-    fragColor = vec4(1.0, 1.0, 1.0, 1.0);
 
+    //fragColor = vec4(1.0, 1.0, 1.0, 1.0);
+
+    vec3 v_color = vec3(1.0, 1.0, 1.0);
+    vec2 point_uv = 2.0 * gl_PointCoord - 1.0;
+
+    float circle = smoothstep(1.0, 0.7, dot(point_uv, point_uv));
+    if (circle < 0.1) discard;
+
+    fragColor = vec4(vec3(circle * v_color), 1);
 }
